26ee84b2469b0e45d53851674b20aff195643a58
Evan Suma Rosenberg <suma@umn.edu>
2022-12-08 16:53:03 -0600

    commit

--- src/ProjectionApp.ts
+++ src/ProjectionApp.ts
@@ -163,7 +163,20 @@ export class ProjectionApp extends gfx.GfxApp
         }
         else
         {
-            //
+            // http://learnwebgl.brown37.net/08_projections/projections_ortho.html
+
+            const translation = new gfx.Vector3();
+            translation.x = 0;
+            translation.y = 0;
+            translation.z = (this.farClip - this.nearClip) / 2;
+
+            const scale = new gfx.Vector3();
+            scale.x = 2 / this.orthoWidth;
+            scale.y = 2 / this.orthoHeight;
+            scale.z = -2 / (this.farClip - this.nearClip);
+
+            this.camera.projectionMatrix.setTranslation(translation);
+            this.camera.projectionMatrix.premultiply(gfx.Matrix4.makeScale(scale));
         }
 
         // Resize the viewport based on the camera aspect ratio
@@ -173,6 +186,13 @@ export class ProjectionApp extends gfx.GfxApp
     // Override the default resize event handler
     resize(): void
     {
-        this.renderer.resize(window.innerWidth, window.innerHeight, this.aspectRatio);
+        if(this.projectionMode == 'Perspective')
+        {
+            this.renderer.resize(window.innerWidth, window.innerHeight, this.aspectRatio);
+        }
+        else
+        {
+            this.renderer.resize(window.innerWidth, window.innerHeight, this.orthoWidth / this.orthoHeight);
+        }
     }
 }
